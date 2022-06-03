fbc703695792cc748e53be9dbf18a55cf4a45bb0
Mohamad Al <mohamada0112@gmail.com>
2022-06-03 10:29:14 +0700

    Add files via upload

---
+++ 2-tipe-data-number.php
@@ -0,0 +1,30 @@
+<?php 
+echo "Decimal: ";
+var_dump(-1234);
+
+echo "Octal: ";
+var_dump(0123);
+
+echo "String: ";
+var_dump("test");
+
+echo "bener: ";
+var_dump(false);
+
+echo <<<'test'
+lala yeyey
+asdasdmwnd j eq qwejqw 
+test;
+
+echo "Underscore in number : ";
+var_dump(1_241_241_241);
+
+echo "Floating Point dengan E Notation : (1.7 x 1000) : ";
+var_dump(1.7e3);
+
+
+#heredoc
+echo <<<BARU
+
+BARU;
+
---
+++ 3-constant.php
@@ -0,0 +1,9 @@
+<?php 
+define("Umur", 100, 200);
+define("JenKel", "Laki-Laki");
+
+echo "Umur :";
+echo Umur;
+echo "\nJemKel :";
+echo JenKel;
+?>
